a2c4e3a6101d914cb0af5bdc3fc6f400fd3a75d3
richard Hu <yongjinhu1230@gmail.com>
2022-05-31 11:09:08 +0800

    card-list-component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import './card-list.styles.css'
 
 class CardList extends Component {
 
@@ -8,11 +9,17 @@ class CardList extends Component {
         const { monsters } = this.props
         return (
             <div className="card-list">
-                {monsters.map((monster) => (
-                    <div key={monster.id}>
-                        <h1>{monster.name}</h1>
-                    </div>
-                ))}
+                {monsters.map((monster) => {
+                    const { name, email, id } = monster;
+                    return (
+                        <div key={id} className='card-container'>
+                            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    )
+
+                })}
             </div>
         )
 
