04827edb0d3436611599b2f7a1d6f391fcaab048
richard Hu <yongjinhu1230@gmail.com>
2022-06-01 20:24:26 +0800

    useEffect_02

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component'
 import './App.css'
@@ -6,46 +6,40 @@ import './App.css'
 
 const App = () => {
 
-
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters,setFilteredMonsters] = useState(monsters)
-
-
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then(users => setMonsters(
-        { monsters: users.filter(item => item.id !== 10) }
+        // { monsters: users }//这么写是有问题的
+        users
       ))
   }, [])
 
-
-  useEffect(() => { 
+  useEffect(() => {
     const newFilteredMonsters = monsters.filter(
       (monster) => {
-        return monster.name.toLowerCase().includes(searchField)
+        return monster.name.toLocaleLowerCase().includes(searchField)
         //注意:filter也是返回对象,这里的逻辑是筛选出输入框中外部输出的包含字符串的name
       });
-      setFilteredMonsters(newFilteredMonsters)
+    setFilteredMonsters(newFilteredMonsters)//这里会导致filteredMonsters发生变化
 
-  },[monsters,searchField])
+  }, [monsters, searchField])
 
 
   const onSearchChange = (e) => {
-    const searchFieldString = e.target.value.toLowerCase();
-    setSearchField(searchFieldString)
+    const searchFieldString = e.target.value.toLocaleLowerCase();
+
+    setSearchField(searchFieldString)//这里会导致searchField发生变化
   }
 
- 
-
-
   return (
-    <div className="App">
+    <div className='App'>
       <h1 className='app-title'>Monsters Rolodex</h1>
-
       <SearchBox
         className='monsters-search-box'
         onChangeHandler={onSearchChange}
@@ -57,58 +51,4 @@ const App = () => {
     </div>
   )
 }
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       monsters: [],
-//       searchField: ''
-//     }
-
-//   }
-
-//   componentDidMount() {
-//     fetch(
-//       'https://jsonplaceholder.typicode.com/users'
-//     ).then((response) => response.json())
-//       .then(users => this.setState(
-//         { monsters: users.filter(item => item.id !== 10) }
-//       )
-//       )
-//   }
-
-//   onSearchChange = (e) => {
-//     const searchField = e.target.value.toLowerCase();
-//     this.setState(
-//       // () => { 
-//       // return {searchField }
-//       // }
-//       //two methods are equal
-//       { searchField: searchField }
-//     )
-//   }
-
-//   render() {
-
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-//     const filterMonsters = monsters.filter(
-//       (monster) => monster.name.toLowerCase().includes(searchField)
-//       //注意:filter也是返回对象,这里的逻辑是筛选出输入框中外部输出的包含字符串的name
-//     )
-//     return (
-//       <div className="App">
-//         <h1 className='app-title'>Monsters Rolodex</h1>
-//         <SearchBox
-//           className='monsters-search-box'
-//           onChangeHandler={onSearchChange}
-//           placeholder='search monsters'
-//         />
-//         <CardList monsters={filterMonsters} />
-//       </div>
-//     );
-//   }
-// }
-
 export default App
