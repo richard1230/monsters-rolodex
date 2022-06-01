ba548c7c6b63dce59f73a6e01eff7a9a6f896890
richard Hu <yongjinhu1230@gmail.com>
2022-06-02 00:09:15 +0800

    finish_00

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,26 +1,15 @@
-import { Component } from "react";
 import './card-list.styles.css'
 import Card from "../card/card.component";
 
-class CardList extends Component {
-
-
-    render() {
-        const { monsters } = this.props;
-        return (
-            <div className="card-list">
-                {monsters.map((monster) => <Card monster={monster} key={ monster.id}/>)}
-            </div>
-        )
-
-    }
+const CardList = (props) => {
+    const { monsters } = props
+    return (
+        <div className="card-list">
+            {
+                monsters.map((monster) =>
+                    <Card monster={monster} key={monster.id} />)}
+        </div>
+    )
 }
 
-
 export default CardList;
-
-
-/*
-{monsters.map((monster) => { return <Card monster={ monster}/>})}
-{monsters.map((monster) => <Card monster={ monster}/>)}
-*/
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,19 +1,15 @@
-import { Component } from "react";
 import './card.style.css'
 
+const Card = (props) => {
+    const { name, email, id } = props.monster;
 
-class Card extends Component { 
-    render() { 
-                const { name, email, id } = this.props.monster;
-                return (
-                    <div key={id} className='card-container'>
-                        <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-                        <h2>{name}</h2>
-                        <p>{email}</p>
-                    </div>
-                )
-    }
+    return (
+        <div key={id} className='card-container'>
+            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
 }
-
 
 export default Card;
